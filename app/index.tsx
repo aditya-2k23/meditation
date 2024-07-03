75d1fb59bdee9229e9635537a1d00704fab74a30
Aditya <holaaditya123@gmail.com>
2024-07-03 22:58:21 +0530

    chore: Update app/index.tsx to include image background and linear gradient

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,38 @@
-import { Text, View } from "react-native";
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
 
-export default function HomeScreen() {
+import beachImage from "../assets/meditation-images/beach.webp";
+
+const App = () => {
   return (
-    <View className="container w-full h-screen bg-black flex justify-center items-center">
-      <Text className="text-white font-sans text-3xl">Hello World</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+        >
+          <SafeAreaView className="flex-1 px-1 justify-between">
+            <View>
+              <Text className="text-center text-white font-semibold text-4xl">
+                Simple Meditation
+              </Text>
+              <Text className="text-center text-white text-2xl mt-3">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+
+            <StatusBar style="light"></StatusBar>
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
-}
+};
+
+export default App;
