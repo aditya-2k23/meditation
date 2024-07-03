60e17d88af667aa5d99de0555d5776db706b15b6
Aditya <holaaditya123@gmail.com>
2024-07-03 23:50:00 +0530

    chore: Update Content component to use py-12 for padding

--- components/Content.tsx
+++ components/Content.tsx
@@ -2,9 +2,7 @@ import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 
 const Content = ({ children }: any) => {
-  return (
-    <SafeAreaView className="flex-1 px-5 pt-8 pb-4">{children}</SafeAreaView>
-  );
+  return <SafeAreaView className="flex-1 px-5 py-12">{children}</SafeAreaView>;
 };
 
 export default Content;
