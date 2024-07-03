fe7ca55317ec71e3d55d76ab27be7f9bac8e0f3e
Aditya <holaaditya123@gmail.com>
2024-07-04 00:06:53 +0530

    refactor: Update _layout.tsx to use Tabs instead of Stack for routing

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,32 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import Colors from "@/constants/Colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: Colors.primary,
+      }}
+    >
+      <Tabs.Screen
+        name="NatureMeditate"
+        options={{
+          tabBarLabel: "Meditate",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="flower-tulip"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
