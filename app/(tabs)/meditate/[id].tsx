3efbdec82829b865525e167780bac176fb8dd244
Aditya <holaaditya123@gmail.com>
2024-07-04 13:14:12 +0530

    refactor: Update Meditate/[id] screen to display formatted time in countdown timer

--- app/(tabs)/meditate/[id].tsx
+++ app/(tabs)/meditate/[id].tsx
@@ -32,6 +32,11 @@ const Meditate = () => {
     };
   }, [secondsRemaining, meditating]);
 
+  const formattedTimeMinutes = String(
+    Math.floor(secondsRemaining / 60)
+  ).padStart(2, "0");
+  const formattedTimeSeconds = String(secondsRemaining % 60).padStart(2, "0");
+
   return (
     <View className="flex-1">
       <ImageBackground
@@ -49,8 +54,8 @@ const Meditate = () => {
 
           <View className="flex-1 justify-center">
             <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center">
-              <Text className="text-4xl text-blue-800">
-                00:{secondsRemaining}
+              <Text className="text-4xl text-blue-800 font-rmono">
+                {formattedTimeMinutes}:{formattedTimeSeconds}
               </Text>
             </View>
           </View>
