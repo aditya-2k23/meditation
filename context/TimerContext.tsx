05d44b2ea7efa89c53b93d4f356a204e5aac61b5
Aditya <holaaditya123@gmail.com>
2024-07-05 12:52:53 +0530

    feat: Add TimerContext component
    The commit adds a new component called TimerContext to the project. This component is responsible for managing the duration of the timer used in the meditation feature. It provides a context with the duration value and a function to update it. This addition enhances the modularity and reusability of the codebase.

---
+++ context/TimerContext.tsx
@@ -0,0 +1,32 @@
+import {
+  createContext,
+  useState,
+  type Dispatch,
+  type SetStateAction,
+} from "react";
+
+interface TimerContextType {
+  duration: number;
+  setDuration: Dispatch<SetStateAction<number>>;
+}
+
+export const TimerContext = createContext<TimerContextType>({
+  duration: 10,
+  setDuration: () => {},
+});
+
+interface TimerProviderProps {
+  children: React.ReactNode;
+}
+
+const TimerProvider = ({ children }: TimerProviderProps) => {
+  const [duration, setDuration] = useState(10);
+
+  return (
+    <TimerContext.Provider value={{ duration, setDuration }}>
+      {children}
+    </TimerContext.Provider>
+  );
+};
+
+export default TimerProvider;
