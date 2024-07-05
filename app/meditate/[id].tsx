a77908885dcbd9bd8d7b7f312808a5352493ecfb
Aditya <holaaditya123@gmail.com>
2024-07-05 12:53:37 +0530

    feat: Add TimerContext and useContext hook to manage meditation timer duration
    The code changes in this commit add the TimerContext component and useContext hook to manage the duration of the meditation timer. This enhancement improves the modularity and reusability of the codebase by centralizing the timer duration state and providing a way to update it using the useContext hook.

--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -1,5 +1,5 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MEDITATION_IMAGES from "@/constants/meditation-images";
 import { MEDITATION_DATA, AUDIO_FILES } from "@/constants/MeditationData";
 import AppGradient from "@/components/AppGradient";
@@ -7,10 +7,14 @@ import { router, useLocalSearchParams } from "expo-router";
 import { AntDesign } from "@expo/vector-icons";
 import { Audio } from "expo-av";
 import CustomButton from "@/components/CustomButton";
+import { TimerContext } from "@/context/TimerContext";
 
 const Meditate = () => {
   const { id } = useLocalSearchParams();
-  const [secondsRemaining, setSecondsRemaining] = useState(10);
+
+  const { duration: secondsRemaining, setDuration } = useContext(TimerContext);
+
+  // const [secondsRemaining, setSecondsRemaining] = useState(10);
   const [meditating, setMeditating] = useState(false);
   const [audioSound, setAudioSound] = useState<Audio.Sound>();
   const [playingAudio, setPlayingAudio] = useState(false);
@@ -26,7 +30,7 @@ const Meditate = () => {
 
     if (meditating) {
       timerId = setTimeout(() => {
-        setSecondsRemaining(secondsRemaining - 1);
+        setDuration(secondsRemaining - 1);
       }, 1000);
     }
 
@@ -37,12 +41,13 @@ const Meditate = () => {
 
   useEffect(() => {
     return () => {
+      setDuration(10);
       audioSound?.unloadAsync();
     };
   }, [audioSound]);
 
   const toggleMeditationSessionStatus = async () => {
-    if (secondsRemaining === 0) setSecondsRemaining(10);
+    if (secondsRemaining === 0) setDuration(10);
 
     setMeditating(!meditating);
     await toggleSound();
